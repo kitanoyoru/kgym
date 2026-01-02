862e2c4840f55b6a532ab96d4c9fbc743bbade93
Alexandr Rutkowski <kitanoyoru@icloud.com>
2026-01-02 14:09:34 +0100

    Up terraform modules k8s version

--- infrastructure/modules/kafka/versions.tf
+++ infrastructure/modules/kafka/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
+      version = "~> 3.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.12"
+      version = "~> 3.1"
     }
     null = {
       source  = "hashicorp/null"
